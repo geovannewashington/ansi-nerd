ce6e245c001a6ae1ea7ff49e9fb706520d740867
Geovanne Washington <geoxp98@gmail.com>
2025-01-09 13:08:11 -0300

    Exercise 1-3 DONE

---
+++ Book/09-01-2025/exercise1-3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celcius table for fahr = 0, 20, 40, ..., 300 */
+
+void header()
+{
+	printf("================================\n");
+	printf("----Fahrenheit-Celcius-Table----\n");
+	printf("================================\n");
+}
+
+int main() 
+{	
+	float fahr, celcius;
+	float lower, upper, step;
+
+	lower = 0;      /* lower limit of temperature scale */
+	upper = 300;    /* upper limit */
+	step = 20;      /* step size  */
+	fahr = lower;
+
+ 	header();	
+
+	while (fahr <= upper) {
+		celcius = (5.0 / 9.0 * (fahr - 32.0));
+		printf("%3.0f %6.1f\n", fahr, celcius);
+		fahr = fahr + step;
+	}
+}

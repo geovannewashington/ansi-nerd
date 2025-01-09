8bee3e52888e05d8ea2a227ec4e63531520f8542
Geovanne Washington <geoxp98@gmail.com>
2025-01-09 13:03:34 -0300

    abc

--- Book/13-11-2024/celcius-to-fahr.c
+++ Book/13-11-2024/celcius-to-fahr.c
@@ -4,8 +4,8 @@
 
 int main() 
 {	
-	int fahr, celcius;
-	int lower, upper, step;
+	float fahr, celcius;
+	float lower, upper, step;
 
 	lower = 0;      /* lower limit of temperature scale */
 	upper = 300;    /* upper limit */
@@ -13,8 +13,8 @@ int main()
 	fahr = lower;
 	
 	while (fahr <= upper) {
-		celcius = 5 * (fahr - 32) / 9;
-		printf("%3d %6d\n", fahr, celcius);
+		celcius = (5.0 / 9.0 * (fahr - 32.0));
+		printf("%3.0f %6.1f\n", fahr, celcius);
 		fahr = fahr + step;
 	}
 }

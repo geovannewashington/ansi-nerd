bd9ec4d480f98ab79a09a2252de77780a438e4fe
Geovanne Washington <geoxp98@gmail.com>
2025-02-03 12:33:44 -0300

    Changing exercise 1-5 a bit

--- Book/03-02-2025/exercise1-5.c
+++ Book/03-02-2025/exercise1-5.c
@@ -9,10 +9,12 @@ void table_header()
 
 int main(void)
 {
+        const int start_temp = 300;
+        const int step = 20; 
         int fahr;
 
         table_header();
-        for (fahr = 300; fahr >= 0; fahr -= 20) {
+        for (fahr = start_temp; fahr >= 0; fahr -= step) {
                 printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
         }
 

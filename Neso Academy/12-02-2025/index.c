38358b21065c543f84e80d8048eee78ca245992d
Geovanne Washington <geoxp98@gmail.com>
2025-02-12 14:39:17 -0300

    [DAY 12-02-2025]: About Scopes

---
+++ Neso Academy/12-02-2025/index.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+//function prototype:
+int func();
+
+int main(void)
+{
+        /* -> scope of this variable 'var' is local to main function ONLY! 
+        therefore called LOCAL to main() function. */
+        int var = 30; 
+        
+        printf("%d\n", var);  
+        return 0;
+}
+
+int func()
+{
+        printf("%d\n", var); // -> var is not available here...
+        // VSCODE says: identifier 'var' in undefined
+}
+
+
+/*
+{
+...some A
+        {
+                ...code C
+        }
+...code B
+}
+
+*/
+
+void A() {
+    {   // Code C
+        printf("Value of b: %d\n", b); // ✅ Accessible
+    }
+    int b = 10; // Code B
+}

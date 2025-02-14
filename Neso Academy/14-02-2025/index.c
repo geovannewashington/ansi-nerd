821dcc968a5d436c8862971556601e82f9ca0dcd
Geovanne Washington <geoxp98@gmail.com>
2025-02-14 13:28:43 -0300

    About auto and extern var modifiers

---
+++ Neso Academy/14-02-2025/index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+extern int external_var; // -> this variable is declared here but not defined here
+int global_int; // -> global variables are, by deafult, initalized with zero;
+
+int main(void)
+{
+        auto int my_int;        // -> this is the same as int my_int;
+        printf("%d\n", my_int); // -> will print some gargabe value (random value)
+
+        printf("%d\n", global_int); // -> 0
+
+        printf("%d\n", external_var); // -> 25
+        return 0;
+}

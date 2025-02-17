abf1b348cdc070936bcb62efc5ecf52f9a078510
Geovanne Washington <geoxp98@gmail.com>
2025-02-17 12:28:24 -0300

    Done: exercise1-8

--- Book/16-02-2025/exercise1-8.c
+++ Book/16-02-2025/exercise1-8.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
-/* counts the number of blanks, tabs and newlines */
+/* Counts the number of blanks, tabs and newlines in the input*/
 
 int main(void)
 {
-        int counter, new_lines, new_tabs, new_blanks;
-        counter = 0;
-        
+        int counter; 
+        int new_lines = 0, new_tabs = 0, new_blanks = 0;
+
         while ((counter = getchar()) != EOF) {
-                if (counter = '\n') {
+                if (counter == '\n') {
                         ++new_lines;
-                }
-
-                if (counter = '\t') {
+                } else if (counter == '\t') {
                         ++new_tabs;
-                }
-
-                if (counter = 'blank space') {
+                } else if (counter == ' ') {
                         ++new_blanks;
                 }
         }
 
-
-        printf("new lines inserted: %d\n", new_lines);
-        printf("tabs inserted: %d\n", new_tabs);
-        printf("blank characters  inserted: %d\n", new_blanks);
+        printf("New lines inserted: %d\n", new_lines);
+        printf("Tabs inserted: %d\n", new_tabs);
+        printf("Blank characters  inserted: %d\n", new_blanks);
+        
         return 0;
 }

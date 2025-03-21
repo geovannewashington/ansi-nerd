bbedf6ab7e9f67f0586db53b54eebe706cdd6eb0
Geovanne Washington <geoxp98@gmail.com>
2025-03-21 11:50:58 -0300

    The left-shift bitwise operator now correctly explained

--- Neso Academy/21-03-2025/index.c
+++ Neso Academy/21-03-2025/index.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /* Aula N.28 - BitWise Operators in C [part2] */
-// >> left shift operator 
+// << left shift operator 
 // it's a binary operator meaning it takes two operands 
 
 // SYNTAX: firstOperand << secondOperand
@@ -19,13 +19,13 @@ int main(void)
     // since 3 in bits is 0011 when we shift it left by one it turns into 0110, which represents six in binary
 
     // Important Point:
-    // x << y = x + 2^y 
+    // x << y = x * 2^y 
     // Example:
     
     int b = 6;
-    printf("%d\n", b << 2); // -> Expected Output: 10
+    printf("%d\n", b << 2); // -> Expected Output: 24
     //Explanation:
     // since 6 in bits is 0000 0110 (2¹ + 2²) when we shift it left by two it turns into
-    // 0001 1000, which represents 24 I guess... so apparently the rule x << y = x + 2^y is not always true
+    // 0001 1000, which represents 24 
     return 0;
 }

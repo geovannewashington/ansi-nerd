bc7d3548ce471ca7cf9d1f516cb7f9c3886fcbfe
Geovanne Washington <geoxp98@gmail.com>
2025-03-24 11:41:21 -0300

    assignment operators in c

---
+++ Neso Academy/24-03-2025/index.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Aula n.31 - Assignment Operators in C */
+// A little exercise 
+
+int main(void)
+{
+    char a = 7;
+    a ^= 5;
+    printf("%d\n", printf("%d", a += 3));
+    return 0;
+}

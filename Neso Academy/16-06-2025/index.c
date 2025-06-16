4c2e783fb43435428b9b52a533c31305f3da5397
Geovanne Washington <geoxp98@gmail.com>
2025-06-16 12:17:09 -0300

    homework exercise done

--- Neso Academy/16-06-2025/index.c
+++ Neso Academy/16-06-2025/index.c
@@ -3,6 +3,8 @@
 // So question is: How many times will 'Neso' be printed on the screen?
 // Possibilites are: 10 times, 5 times, infinite times and 0 times.
 
+int my_main(void);
+
 int main(void)
 {
     int i = -5;
@@ -15,7 +17,28 @@ int main(void)
         }
         printf("Neso");
     }
+    my_main();
     return 0;
 }
 
 // 0 times!!
+
+// This is the homework problem.
+// What is the output of the following funciton (my_main)
+int my_main(void) {
+    int i = 0;
+    for (printf("one\n"); i < 3 && printf(""); i++)
+        printf("Hi!\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+

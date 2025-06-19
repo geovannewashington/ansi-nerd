974d15bac14a6065637a43a192169c12a15fa92a
Geovanne Washington <geoxp98@gmail.com>
2025-06-19 12:22:29 -0300

    understood the ascii-tree program logic... sort of

--- Neso Academy/18-06-2025/part2/index.c
+++ Neso Academy/18-06-2025/part2/index.c
@@ -8,18 +8,20 @@ int main(int argc, char *argv[])
     int rows; 
     printf("How many rows do you want in your pyramid? ");
     scanf("%d", &rows);
-  
-    // row's loop
-    for (int i = 1; i <= rows; i++) {
-        // NOTE: 2 * rows - 1 == total quantity of columns for this pyramid   
-        int total_columns = (2 * rows - 1);
 
+    int total_columns = (2 * rows - 1);
+    
+    // row's loop
+    //  NOTE: when i = 0, this is the first row, that is, the pyramid gets printed
+    //  from top to bottom my dear fried.
+    for (int i = 1; i <= rows; i++) {
         // column's loop
         for (int j = 1; j <= total_columns; j++)
-            if (j >= rows-(i-1) && j <= rows+(i+1))
+            if (j >= rows-(i-1) && j <= rows+(i-1))
                 printf("*");
             else 
                 printf(" ");
+        printf("\n");
     }
 
     return 0;

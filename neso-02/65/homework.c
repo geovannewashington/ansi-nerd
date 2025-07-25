c8e544a447429ac0a723462cb5fe0a820b9d853e
Geovanne Washington <geoxp98@gmail.com>
2025-07-25 12:23:30 -0300

    Rightshift operator exercise

--- neso-02/65/homework.c
+++ neso-02/65/homework.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Neso Academy - Class n. 65:
+// Neso Academy - Class n. 65 (HOMEWORK): 
 // What is the output of the following program 
 
 int func(int num)
 {
-    int count = 0;
+    int count = 2;
     while (num) {
         count++;
-        num >>= 1; 
+        num >>= 2; 
     }
     return(count);
 }
 
 int main(void)
 {
-    printf("%d\n", func(435)); // 9
+    printf("%d\n", func(435)); // 5
     exit(EXIT_SUCCESS);
 }
 

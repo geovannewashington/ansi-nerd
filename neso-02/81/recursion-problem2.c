ff143c4772d21c64dd940e96943482cf7c2db26c
Geovanne Washington <geoxp98@gmail.com>
2025-08-04 12:48:06 -0300

    Add exercise: Recursion (Solved Problem 2)

---
+++ neso-02/81/recursion-problem2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Neso Academy - Class n. 81: 
+// Recursion (Solved Problem 2) 
+
+// Determine, how many number of times the star will be printed on the screen.
+
+// This runs k(k + 1)/2 times
+void foo(int n)
+{
+    int i = 0;
+    if (n > 1)
+        foo(n - 1);
+    for (i = 0; i < n; i++) {
+        printf(" * ");
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    foo(5);
+    return 0;
+}

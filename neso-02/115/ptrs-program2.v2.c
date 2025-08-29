6effcf0011352bbb1e4b09fc17347c5fdcad3f75
Geovanne Washington <geoxp98@gmail.com>
2025-08-29 13:39:32 -0300

    Add my version of pointers program example 2

--- neso-02/115/ptrs-program2.v2.c
+++ neso-02/115/ptrs-program2.v2.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define ARR_SIZE 5
 
@@ -11,19 +12,31 @@ int main(void)
 {
     int arr[ARR_SIZE];
     int *ptr = NULL;       
-    
     char buf[1024]; // using 1KiB 
 
     printf("Enter %d elements in the array: ", ARR_SIZE);
-    for (ptr = arr; ptr <= &arr[ARR_SIZE]; ptr++) {
-        if (!fgets(buf, 1024, stdin)) { /* reading input failed */ 
-            return EXIT_FAILURE;
+    
+    if (!fgets(buf, sizeof buf, stdin)) { /* reading input failed */ 
+        fprintf(stderr, "Reading input failed, exiting program.");
+        return EXIT_FAILURE;
+    }
+    
+    char *p = buf; // p = &buf[0];
+    for (ptr = arr; ptr <= &arr[ARR_SIZE - 1]; ptr++) {
+        // Skip spaces/tabs/newlines
+        while (isspace((unsigned char)*p)) {
+            p++;
         }
-        *ptr = atoi(buf);
+        
+        *ptr = atoi(p);
+
+        while (*p && !isspace((unsigned char)*p)) {
+            p++;
+        }
     }
     
     puts("Elements in reverse order:"); 
-    for (ptr = &arr[ARR_SIZE]; ptr >= arr; ptr--) 
+    for (ptr = &arr[ARR_SIZE - 1]; ptr >= arr; ptr--) 
         printf("%d ", *ptr);
    
     printf("\n");

b8f876b63fe588a26d74fc8dcd1f5b773103492e
Geovanne Washington <geoxp98@gmail.com>
2025-09-01 13:50:45 -0300

    Arithmetic of multidimensional arrays - part1

---
+++ neso-02/118/arithmetic-mult-arr.c
@@ -0,0 +1,29 @@
+// Neso Academy - Class n. 118: 
+// Processing the Multidimensional Array Elements (or) Address Arithmetic of Multidimensional Arrays 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 2d and 3d (and higher-dimensional arrays) still contiguous in memory.
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// 2D Array
+// int a[2][2]; -> two 1d arrays, each of which contains 2 elements
+
+int main(void) 
+{
+    int arr[2][2] = {
+        { 10, 20 },
+        { 30, 40 },
+    };
+    
+    // When using arr it will decay the a pointer of the first 1d array
+    // Which means that arr + 1 will point to the second 1d array
+    printf("%p\n", arr + 1); // same address of printf("%p\n", &arr[1][0]);  
+    printf("%p\n", &arr[1][0]);
+    
+    // so if we want to access the element itself:
+    // **arr => *(*arr) => *(*arr[0]) => *(&arr[0][0]) => a[0][0] = 10
+    printf("%d\n", **arr);
+    return EXIT_SUCCESS;
+}
